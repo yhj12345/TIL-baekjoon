9d123557b8b5ce9bbc16a3a667bb99ff55364d82
HJ <95677084+yhj12345@users.noreply.github.com>
2023-01-08 17:18:49 +0900

    [Gold III] Title: 방 번호, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -0,0 +1,14 @@
+import sys
+
+N = int(input())
+P = list(map(int, input().split()))
+M = int(input())
+
+dp = [-1000000 for _ in range(M+1)]
+
+for i in range(N-1, -1, -1):
+  now = P[i]
+  for j in range(now, M+1):
+    dp[j] = max(dp[j-now]*10+i, i, dp[j])
+
+print(dp[-1])
