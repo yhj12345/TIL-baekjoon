42d7f3c02cca61fa2366c1fa9310acc1e292bb5b
HJ <95677084+yhj12345@users.noreply.github.com>
2023-01-10 21:14:19 +0900

    [Gold V] Title: A와 B, Time: 120 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,16 @@
+import sys
+
+S = list(input())
+T = list(input())
+
+while len(T) > len(S):
+  if T[-1] == 'A':
+    T.pop()
+  else:
+    T.pop()
+    T.reverse()
+
+if ''.join(S) == ''.join(T):
+  print(1)
+else:
+  print(0)
