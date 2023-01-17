8e9673b5031598935a7a0b034611a168f1dad948
HJ <95677084+yhj12345@users.noreply.github.com>
2023-01-17 23:10:30 +0900

    [Gold IV] Title: 개근상, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/1563. 개근상/개근상.py
+++ 백준/Gold/1563. 개근상/개근상.py
@@ -5,10 +5,10 @@ dp = [[0 for _ in range(6)] for _ in range(N+1)]
 dp[0][0] = 1
 
 for i in range(1, N+1):
-  dp[i][0] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2]
+  dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % 1000000
   dp[i][1] = dp[i-1][0]
   dp[i][2] = dp[i-1][1]
-  dp[i][3] = dp[i-1][0] + dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4] + dp[i-1][5] 
+  dp[i][3] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4] + dp[i-1][5]) % 1000000 
   dp[i][4] = dp[i-1][3]
   dp[i][5] = dp[i-1][4]
 print(sum(dp[N]) % 1000000)
