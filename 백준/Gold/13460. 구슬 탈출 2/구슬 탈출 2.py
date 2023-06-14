63e6999ec9eb114aa25a41fcb41901e409246da0
HJ <95677084+yhj12345@users.noreply.github.com>
2023-06-15 01:34:16 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 164 ms, Memory: 116424 KB -BaekjoonHub

---
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -0,0 +1,97 @@
+from collections import deque
+
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+def bfs(rx, ry, bx, by):
+    q = deque()
+    visited[rx][ry][bx][by] = 1
+    q.append((rx, ry, bx, by))
+
+    while q:
+        ri, rj, bi, bj = q.popleft()
+
+        if visited[ri][rj][bi][bj] == 11:
+            return -1
+
+        for k in range(4):
+            tri, trj, tbi, tbj = ri, rj, bi, bj
+            arr[ri][rj] = 'R'
+            arr[bi][bj] = 'B'
+            flag = False
+            isRedGoal = False
+            isBlueGoal = False
+            while True:
+                nri = tri + di[k]
+                nrj = trj + dj[k]
+                if arr[nri][nrj] == '#':
+                    break
+                elif arr[nri][nrj] == 'B':
+                    flag = True
+                    break
+                elif arr[nri][nrj] == '.':
+                    arr[tri][trj] = '.'
+                    arr[nri][nrj] = 'R'
+                    tri, trj = nri, nrj
+                elif arr[nri][nrj] == 'O':
+                    arr[tri][trj] = '.'
+                    isRedGoal = True
+                    break
+
+            while True:    
+                nbi = tbi + di[k]
+                nbj = tbj + dj[k]
+
+                if arr[nbi][nbj] == '#' or arr[nbi][nbj] == 'R':
+                    break
+                elif arr[nbi][nbj] == '.':
+                    arr[tbi][tbj] = '.'
+                    arr[nbi][nbj] = 'B'
+                    tbi, tbj = nbi, nbj
+                elif arr[nbi][nbj] == 'O':
+                    arr[tbi][tbj] = '.'
+                    isBlueGoal = True
+                    break
+                
+            if flag:
+                while True:
+                    nri = tri + di[k]
+                    nrj = trj + dj[k]
+                    if arr[nri][nrj] == '#' or arr[nri][nrj] == 'B':
+                        break
+                    elif arr[nri][nrj] == '.':
+                        arr[tri][trj] = '.'
+                        arr[nri][nrj] = 'R'
+                        tri, trj = nri, nrj
+                    elif arr[nri][nrj] == 'O':
+                        arr[tri][trj] = '.'
+                        isRedGoal = True
+                        break
+                    
+            if isRedGoal and not isBlueGoal:
+                return visited[ri][rj][bi][bj]
+            
+            elif not isRedGoal and not isBlueGoal and visited[tri][trj][tbi][tbj] == 0:
+                visited[tri][trj][tbi][tbj] = visited[ri][rj][bi][bj] + 1
+                q.append((tri, trj, tbi, tbj))
+            arr[tri][trj] = '.'
+            arr[tbi][tbj] = '.'
+        
+    return -1
+
+
+N, M = map(int, input().split())
+arr = [list(input()) for _ in range(N)]
+visited = [[[[0 for _ in range(M)] for _ in range(N)] for _ in range(M)] for _ in range(N)]
+
+ri, rj , bi, bj = 0, 0, 0, 0
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 'R':
+            ri, rj = i, j
+            arr[i][j] = '.'
+        elif arr[i][j] == 'B':
+            bi, bj = i, j
+            arr[i][j] = '.'
+result = bfs(ri, rj, bi, bj)
+print(result)
