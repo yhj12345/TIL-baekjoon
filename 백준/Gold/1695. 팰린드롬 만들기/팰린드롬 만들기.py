1ba54b144a6d7d8fa84ce5d3380c6e547aea73f5
HJ <95677084+yhj12345@users.noreply.github.com>
2023-07-21 20:36:01 +0900

    [Gold IV] Title: 팰린드롬 만들기, Time: 768 ms, Memory: 310720 KB -BaekjoonHub

--- 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Gold/1695. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,11 +1,10 @@
 N = int(input())
-arr1 = list(map(int, input().split()))
-arr2 = arr1[::-1]
+arr = list(map(int, input().split()))
 dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
 
 for i in range(1, N+1):
     for j in range(1, N+1):
-        if arr1[i-1] == arr2[j-1]:
+        if arr[i-1] == arr[N-j]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
